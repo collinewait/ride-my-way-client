42b13348ae2e06126cdf32009b25901651b84e77
Waitire Colline <collinewait17@gmail.com>
2018-07-26 14:48:18 +0300

    Add checks to display correct data to the user

--- js/all_rides.js
+++ js/all_rides.js
@@ -1,4 +1,3 @@
-//import {Alert} from './dialogs.js';
 import {myCookie} from './cookie_file.js';
 
 document.getElementById('defaultOpen').addEventListener('click', getAllRides);
@@ -17,21 +16,33 @@ function getAllRides(){
     })
         .then((res) => res.json())
         .then((data) => {
-            let tableRows = '';
-            data.rides.forEach(ride => {
-                tableRows += `
-                    <tr>
-                        <td>${ride.driver_name}</td>
-                        <td>${ride.driver_id}</td>
-                        <td>
-                            <button class="view_details">View Details</button>
-                        </td>
-                    </tr>
-                `;
-            });
-            loader.style.display = 'none';
-            document.getElementById('rides_data').innerHTML = tableRows;
-            makeDetailsModelActive();
+            const message  = 'results retrieved successfully';
+            if (data.message === message){
+                if(data.rides.length > 0){
+                    let tableRows = '';
+                    data.rides.forEach(ride => {
+                        tableRows += `
+                            <tr>
+                                <td>${ride.driver_name}</td>
+                                <td>${ride.driver_id}</td>
+                                <td>
+                                    <button class="view_details">View Details</button>
+                                </td>
+                            </tr>
+                        `;
+                    });
+                    loader.style.display = 'none';
+                    document.getElementById('rides_data').innerHTML = tableRows;
+                    makeDetailsModelActive();
+                }else{
+                    loader.style.display = 'none';
+                    document.getElementById('all_rides_div').innerHTML = '<h2>No rides currently available</h2>';
+                }
+            
+            }else{
+                
+                window.location.href = 'index.html';
+            }
         });
 }
 
@@ -42,7 +53,6 @@ function makeDetailsModelActive() {
 
     // Get the button that opens the modal
     let btns = document.getElementsByClassName('view_details');
-    console.log(btns);
     
     // Get the <span> element that closes the modal
     const span = document.getElementsByClassName('close')[0];
@@ -52,12 +62,9 @@ function makeDetailsModelActive() {
     
     // When the user clicks on the button, open the modal
     [].forEach.call(btns, (el) => {
-        console.log(el);
         el.addEventListener('click', () => {
             modal.style.display = 'block';
         });
-        //el.click();
-        //console.log("el clicks");
     });
     
     // When the user clicks on the button, close the modal
